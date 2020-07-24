69127a4664df7e59d0ae82915057ce939b08c422
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-25 00:39:37 +0800

    [Story5][SuperSmartParkingBoy][Jim]feature:should_return_2_parked_in_1st_4_parked_in_2nd_when_parking_6_cars_given_1st_parking_lot_max_5_and_2nd_parking_lot_max_10

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -6,23 +6,28 @@ import java.util.UUID;
 
 public class ParkingLot {
     private String parkingLotID;
-    private static final int maxPosition = 10;
+    private int maxPosition;
     private List<Car> parkedCar;
     private int currentUsedPosition;
     ParkingLot(){
         this.parkingLotID = String.valueOf(UUID.randomUUID());
         this.parkedCar = new LinkedList<>();
         this.currentUsedPosition = 0;
+        this.maxPosition = 10;
     }
 
     public ParkingLot(int i) {
+        this.parkingLotID = String.valueOf(UUID.randomUUID());
+        this.parkedCar = new LinkedList<>();
+        this.currentUsedPosition = 0;
+        this.maxPosition = i;
     }
 
     public int getCurrentUsedPosition() {
         return currentUsedPosition;
     }
 
-    public static int getMaxPosition() {
+    public int getMaxPosition() {
         return maxPosition;
     }
 
--- src/main/java/SuperSmartParkingBoy.java
+++ src/main/java/SuperSmartParkingBoy.java
@@ -1,7 +1,39 @@
+import Car.Car;
+
 import java.util.LinkedList;
 
 public class SuperSmartParkingBoy extends ParkingBoy{
     public SuperSmartParkingBoy(LinkedList<ParkingLot> parkingLots) {
         super(parkingLots);
+    }
+
+    @Override
+    public Ticket park(Car car) {
+        if (isAllParkingLotsFull()) {
+            this.setResponseMessage("Not enough position.");
+            return null;
+        }
+        if (isCarParked(car) || car == null) {
+            return null;
+        }
+        Ticket ticket = new Ticket(car.getCarNumber());
+        ParkingLot largestAvailablePositionRateLot = findLargestAvailablePositionRate();
+        largestAvailablePositionRateLot.park(car);
+        return ticket;
+    }
+
+    private ParkingLot findLargestAvailablePositionRate() {
+        Double maxRateOfEmptyParkingLot = 0.0;
+        ParkingLot largestAvailablePositionRateLot = null;
+        for (ParkingLot parkingLot : this.getParkingLots()) {
+            int maxPosition = parkingLot.getMaxPosition();
+            int remainPosition = maxPosition - parkingLot.getCurrentUsedPosition();
+            Double remainEmptyPositionRate = (double)remainPosition/ (double)maxPosition;
+            if (remainEmptyPositionRate > maxRateOfEmptyParkingLot){
+                maxRateOfEmptyParkingLot=remainEmptyPositionRate;
+                largestAvailablePositionRateLot = parkingLot;
+            }
+        }
+        return largestAvailablePositionRateLot;
     }
 }
