703d06df4a1c84948c678c21a7095139a1ad785e
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-24 17:09:55 +0800

    [ParkingBoy][Jim]test&feature:should_return_right_car_when_after_fetching_given_parking_three_cars_and_a_ticket

--- src/test/java/ParkingBoyTest.java
+++ src/test/java/ParkingBoyTest.java
@@ -38,4 +38,38 @@ public class ParkingBoyTest {
             assertFalse(true);
         }
     }
+
+    @Test
+    public void should_return_right_car_when_after_fetching_given_parking_three_cars_and_a_ticket() {
+        // given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car1 = new Car("A1234");
+        Car car2 = new Car("B5678");
+        Car car3 = new Car("C6789");
+        Ticket ticket1 = parkingBoy.park(car1);
+        Ticket ticket2 = parkingBoy.park(car2);
+        Ticket ticket3 = parkingBoy.park(car3);
+
+        // when
+        Car resultCar1 = parkingBoy.fetch(ticket1);
+        Car resultCar3 = parkingBoy.fetch(ticket3);
+        Car resultCar2 = parkingBoy.fetch(ticket2);
+
+        // then
+        if (resultCar1 != null) {
+            assertEquals("A1234", resultCar1.getCarNumber());
+        } else {
+            assertFalse(true);
+        }
+        if (resultCar2 != null) {
+            assertEquals("B5678", resultCar2.getCarNumber());
+        } else {
+            assertFalse(true);
+        }
+        if (resultCar3 != null) {
+            assertEquals("C6789", resultCar3.getCarNumber());
+        } else {
+            assertFalse(true);
+        }
+    }
 }
